065e8430ecca6eb8cd40744254803d7a16912d5a
eduard <kuchmel.eded@mail.ru>
2022-07-15 14:45:20 +0300

    First commit

---
+++ Homework2/Homework2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Factorial
+{
+    internal class Homework2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, факториал которого нужно вычислить");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Факториал {number}: {Factorial(number)}");
+        }
+
+        static int Factorial(int n)
+        {
+            if (n == 1) return 1;
+            return n * Factorial(n - 1);
+        }
+    }
+}
