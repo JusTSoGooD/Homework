8f1d59e584b492866d647efc51e47464dc9e5386
eduard <kuchmel.eded@mail.ru>
2022-07-19 20:07:53 +0300

    Added third homework

---
+++ Homework3/ABCtask/ABCtask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ABCtask
+{
+    internal class ABCtask
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число A");
+            int aNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число B");
+            int bNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число C");
+            int cNumber = int.Parse(Console.ReadLine());
+            if (aNumber >= 100 && bNumber >= 100)
+            {
+                Console.WriteLine("Числа А и Б больше 100");
+            }
+
+            if ((aNumber % 2 == 0 && bNumber % 2 != 0) || (bNumber % 2 == 0 && aNumber % 2 != 0))
+            {
+                Console.WriteLine("Только одно из чисел А и Б четное");
+            }
+
+            if ((aNumber > 0 && bNumber < 0) || (bNumber > 0 && aNumber < 0))
+            {
+                Console.WriteLine("Одно из чисел А и Б положительное");
+            }
+
+            if (aNumber % 3 == 0 && bNumber % 3 == 0 && cNumber % 3 == 0)
+            {
+                Console.WriteLine("Каждое из чисел А Б и С кратно трем");
+            }
+
+            if ((aNumber < 50 && bNumber > 50 && cNumber > 50) || (aNumber > 50 && bNumber < 50 && cNumber > 50) || (aNumber > 50 && bNumber > 50 && cNumber < 50)) 
+            {
+                Console.WriteLine("Только одно из чисел А Б и С меньше 50");
+            }
+
+            if (aNumber < 0 || bNumber < 0 || cNumber < 0)
+            {
+                Console.WriteLine("Одно из чисел A Б или С отрицательное");
+            }
+        }
+    }
+}
---
+++ Homework3/Calculator/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DZ3
+{
+    internal class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1 число");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите 2 число");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите знак арифметической операции");
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    {
+                        Console.WriteLine($"Результат выражения {firstNumber} + {secondNumber} : {firstNumber + secondNumber}");
+                        break;
+                    }
+                case "-":
+                    {
+                        Console.WriteLine($"Результат выражения {firstNumber} - {secondNumber} : {firstNumber - secondNumber}");
+                        break;
+                    }
+                case "/":
+                    {
+                        Console.WriteLine($"Результат выражения {firstNumber} / {secondNumber} : {firstNumber / secondNumber}");
+                        break;
+                    }
+                case "*":
+                    {
+                        Console.WriteLine($"Результат выражения {firstNumber} * {secondNumber} : {firstNumber * secondNumber}");
+                        break;
+                    }
+                case "^":
+                    {
+                        Console.WriteLine($"Результат выражения {firstNumber} ^ {secondNumber} : {Math.Pow(firstNumber, secondNumber)}");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Неизвестная математическая операция");
+                        break;
+                    }
+            }
+        }
+    }
+}
