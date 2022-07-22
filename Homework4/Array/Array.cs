2de1d7de141333873eb7dc45697ab4fe0d870b7d
eduard <kuchmel.eded@mail.ru>
2022-07-22 15:31:48 +0300

    Homework 4

---
+++ Homework4/Array/Array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework4
+{
+    internal class Array
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов массива");
+            int[] array = new int[int.Parse(Console.ReadLine())];
+            int sum = 0;           
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Введите {i + 1} элемент");
+                array[i] = int.Parse(Console.ReadLine());
+                if (i % 2 != 0)
+                {
+                    sum += array[i];
+                }
+            }
+
+            Console.WriteLine($"Сумма элементов с нечетными индексами: {sum}");
+            Console.WriteLine($"Минимальный элемент: {array.Min()}");
+        }
+    }
+}
---
+++ Homework4/StackArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace Homework4
+{
+    internal class StackArray
+    {
+        static void Main(string[] args)
+        {
+
+            var rand = new Random();
+            var array = new int[rand.Next(2, 15)];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 100);
+            }
+
+            var stack = new Stack(array);
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
